26e3c0e1093f107b56dab30eaba11a36d271dd4d
Sireken <43463273+Sireken@users.noreply.github.com>
2018-09-28 15:48:23 +0200

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,6 +2,5 @@
 int main()         // C++ programs start by executing the function main
 {
          cout << "Hello, World!\n";    // output “Hello, World!”
-         keep_window_open();           // wait for a character to be entered
          return 0;
 }
